7cff0236414dfaa418081eda8953cce5b08dae88
SamerArkab <SamerR93@hotmail.com>
2023-06-04 17:28:12 +0300

    added server's ip

--- src/DisplayResults.js
+++ src/DisplayResults.js
@@ -19,7 +19,7 @@ function DisplayResults() {
         calculateSum(bigArray);
 
         // Delete all saved images
-        fetch('http://localhost:5000/api/deleteAll', {
+        fetch('http://34.133.130.136:5000/api/deleteAll', {
             method: 'GET',
         })
             .catch(error => {
